67533e7a3c6d3e0950647f5cd549e22d026ee067
Paras Bhattrai <sivssdn@gmail.com>
2020-10-21 16:56:58 +0100

    generic array.filter added

--- cmd/main.go
+++ cmd/main.go
@@ -9,11 +9,21 @@ import (
 type Array = p.Array
 
 func main() {
-	var testIndex Array = Array{[]string{"e1", "e2", "e3"}}
+	var testIndex Array = Array{[]string{"e1", "e2", "e32", "e14"}}
 	fmt.Println(testIndex.IndexOf("e2"))
+
+	//filter example
+	fmt.Println(testIndex.Filter(func(i interface{}) bool {
+		return len(i.(string)) > 2
+	}))
 
 	testIndex = Array{[]int{1, 12, 300, 50}}
 	fmt.Println(testIndex.IndexOf(50))
+
+	//filter example
+	fmt.Println(testIndex.Filter(func(i interface{}) bool {
+		return i.(int)%2 == 0
+	}))
 }
 
 //TODO:: Implement with generics
--- pkg/filter.go
+++ pkg/filter.go
@@ -1,5 +1,17 @@
 package pkg
 
-// func (list *Array) Filter(condition func) interface{} {
+import (
+	"reflect"
+)
 
-// }
+func (list *Array) Filter(condition func(x interface{}) bool) interface{} {
+	dataType := reflect.TypeOf(list.Value)
+	data := reflect.ValueOf(list.Value)
+	result := reflect.MakeSlice(dataType, 0, 0)
+	for i := data.Len() - 1; i >= 0; i-- {
+		if condition(data.Index(i).Interface()) {
+			result = reflect.Append(result, data.Index(i))
+		}
+	}
+	return result
+}
